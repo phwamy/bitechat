a50e0c5a36415bd3c0e4aaaf56e21176bad45171
phwamy <antilaze@gmail.com>
2024-03-11 18:26:04 -0700

    update langchain agent

--- app.py
+++ app.py
@@ -1,84 +1,124 @@
 import streamlit as st
-import random
-import time
-import biteChatRAG
-from openai import OpenAI
-import os
-from dotenv import load_dotenv
+from bitechat_elasticsearch import chat
+import uuid
 
-load_dotenv()
+def initialize_session_state():
+    if 'session_id' not in st.session_state:
+        st.session_state.session_id = str(uuid.uuid4()) 
+    if 'filter' not in st.session_state:
+        st.session_state.filter = []
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-# Initialize OpenAI with your API key
-field_id = os.getenv('FIELD_ID')
-assistant_id = os.getenv('ASSISTANT_ID')
-api_key = os.getenv('API_KEY')
-client=OpenAI(api_key = api_key)
+def setup_page_layout():
+    st.set_page_config(layout="wide")
+    logo_html = """<img src="https://github.com/phwamy/bitechat/blob/main/bitechat_logo.png?raw=true" alt="Logo" width="70" height="70">"""
+    col1, col2 = st.columns([1, 8])  # Adjust the ratio as needed
+    with col1:
+        st.markdown(logo_html, unsafe_allow_html=True)
+    with col2:
+        st.title("BiteChat")
 
-assistant = biteChatRAG.BiteChaAssistant(client, assistant_id)
+def setup_sidebar():
+    # Initialize the 'filter' key in session_state if it doesn't already exist
+    if 'filter' not in st.session_state:
+        st.session_state.filter = []
 
-# Layout configuration for full width
-st.set_page_config(layout="wide")
+    # Function to update the session state based on checkbox interactions
+    def update_filter(option, is_checked):
+        if is_checked:
+            if option not in st.session_state.filter:
+                st.session_state.filter.append(option)
+        else:
+            if option in st.session_state.filter:
+                st.session_state.filter.remove(option)
 
-# Assuming you've saved your logo as 'logo.png' in your project directory
-logo_html = """<img src="https://github.com/phwamy/bitechat/blob/main/bitechat_logo.png?raw=true" alt="Logo" width="70" height="70">"""
+    with st.sidebar:
+        st.write("## Price")
+        price_options = {"$": "price level inexpensive", "$$": "price level moderate", "$$$": "price level expensive"}
+        for price, label in price_options.items():
+            if st.checkbox(price, key=f"price_{price}"):
+                update_filter(label, True)
+            else:
+                update_filter(label, False)
+        
+        st.write("## Dining Options")
+        dining_options = {"Dine-in": "Dine-in", "Takeout": "Takeout", "Delivery": "Delivery", "Curbside Pickup": "Curbside Pickup"}
+        for option, label in dining_options.items():
+            if st.checkbox(option, key=f"dining_{option}"):
+                update_filter(label, True)
+            else:
+                update_filter(label, False)
+        
+        st.write("## Dietary Preferences")
+        for preference in ["Vegetarian", "Vegan", "Gluten-Free"]:
+            if st.checkbox(preference, key=f"diet_{preference}"):
+                update_filter(preference, True)
+            else:
+                update_filter(preference, False)
+        
+        st.write("## Atmosphere")
+        atmosphere_options = {
+            "Kid-Friendly": "good for children",
+            "Pet-Friendly": "allows dogs",
+            "Sports Bar": "good for watching sports",
+            "Live Music": "Live Music",
+            "Large Groups": "good for groups"
+        }
+        for atmosphere, label in atmosphere_options.items():
+            if st.checkbox(atmosphere, key=f"atmos_{atmosphere}"):
+                update_filter(label, True)
+            else:
+                update_filter(label, False)
+        
+        st.write("## Essential Information")
+        for info in ["Accessibility", "Parking"]:
+            if st.checkbox(info, key=f"info_{info}"):
+                update_filter(info, True)
+            else:
+                update_filter(info, False)
 
-col1, col2 = st.columns([1, 8])  # Adjust the ratio as needed
+        # Debugging: Display current filters
+        # st.sidebar.write("### Current Filters:")
+        # st.sidebar.write(st.session_state.filter)
 
-with col1:
-    st.markdown(logo_html, unsafe_allow_html=True)
-with col2:
-    st.title("BiteChat")
+# Display chat history
+def display_chat_history():
+    for message in st.session_state.messages: 
+        if message["role"] == "assistant":
+            st.chat_message("assistant").write(message["content"])
+        else:
+            st.chat_message("user").write(message["content"])
+
+# Chat handler
+def handle_chat():
+    with st.spinner("Spooning..."):
+        if "message" not in st.session_state:
+            st.session_state.message = []
+
+        for message in st.session_state.message:
+            with st.chat_message(message["role"]):
+                st.markdown(message['content'])
+
+        if prompt := st.chat_input("Seeking any food suggestion?"):
+            st.session_state.messages.append({"role": "user", "content": prompt + str(st.session_state.filter)})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                response = ""
+                response = chat(prompt, st.session_state.session_id)['output']
+                message_placeholder.markdown(response)
+
+            st.session_state.messages.append({"role": "assistant", "content": response})
 
 
+def main():
+    initialize_session_state()
+    setup_page_layout()
+    setup_sidebar()
+    display_chat_history()
+    handle_chat()
 
-# Adding a sidebar for filters and logo at the top of the main page
-with st.sidebar:
-    st.title("Filters")
-    # Example filter: Cuisine Type
-    cuisine_type = st.selectbox("Choose a cuisine", ["Any", "Italian", "Mexican", "Chinese", "Indian"])
-    
-    # Food Type
-    food_type = st.selectbox("Food Type", ["Any", "Vegan", "Non-Veg", "Gluten-Free"])
-    
-    # Restaurant Type
-    restaurant_type = st.selectbox("Restaurant Type", ["Any", "Cafe", "Bistro", "Fine Dining", "Fast Food"])
-    
-    # Dine In or Takeaway
-    dining_option = st.radio("Dining Option", ["Any", "Dine-In", "Takeaway"])
-    
-    # Pet Friendly
-    pet_friendly = st.checkbox("Pet Friendly")
-    
-    # Children Friendly
-    children_friendly = st.checkbox("Children Friendly")
-
-
-
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message_container():
-        st.chat_message(message=message["content"], is_user=message["role"] == "user")
-
-# Accept user input
-with st.spinner("Spooning..."):
-    if "message" not in st.session_state:
-        st.session_state.message = []
-
-    for message in st.session_state.message:
-        with st.chat_message(message["role"]):
-            st.markdown(message['content'])
-
-    if prompt := st.chat_input("Seeking any food suggestion?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            response = ""
-            response = assistant.run_bitechat(prompt)
-            message_placeholder.markdown(response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+if __name__ == "__main__":
+    main()
---
+++ bitechat_elasticsearch.py
@@ -0,0 +1,215 @@
+from langchain.tools import BaseTool
+import googlemaps
+from elasticsearch import Elasticsearch
+from langsmith import Client
+from langchain.callbacks import LangChainTracer
+import os
+from typing import List, Dict, Optional, Type
+from sentence_transformers import SentenceTransformer
+# from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+cloud_id = os.getenv('ElasticSearch_ID')
+if not cloud_id.endswith("=="):
+    cloud_id += "=="
+print(cloud_id)
+
+## Coordinate Search Tool
+class CoordinateSearchTool(BaseTool):
+    name = "Coordinate_Search"
+    description = """Use this tool to search for the coordinates of a location.
+    """
+
+    def _run(self, location: str):
+        """
+        Search for the coordinates of a location.
+
+        Args:
+            location (str): The location to search for.
+
+        Returns:
+            tuple: A tuple containing the latitude and longitude of the location.
+        """
+        gmpas = googlemaps.Client(key=os.getenv('GOOGLE_API'))
+        print(location)
+        geocode_result = gmpas.geocode(location)
+        if geocode_result:
+            print(geocode_result[0]['geometry']['location'])
+            lat = geocode_result[0]['geometry']['location']['lat']
+            lng = geocode_result[0]['geometry']['location']['lng']  
+            return lat, lng
+        else:
+            print('No place result found')
+            lat = None
+            lng = None
+            return lat, lng
+        
+    def _arun(location: str):
+        raise NotImplementedError("This tool does not support async")
+    
+
+## Elasticsearch query tool
+class ElasticSearchToolSchema(BaseModel):
+    query: str = Field(..., description="The query string to search for")
+    lat: Optional[float] = Field(None, description="Latitude for geo-location based search")
+    lon: Optional[float] = Field(None, description="Longitude for geo-location based search")
+    pass
+
+class ElasticSearchTool(BaseTool):
+    args_schema: Type[BaseModel]=ElasticSearchToolSchema
+    name = "Elasticsearch_Query_Tool"
+    description = """
+    Use this tool to query Elasticsearch for restaurant information. Input a dictionary with query, lat (optional), lon(optional).
+    """
+    
+    def _run(self, **kwargs: str) -> List[Dict]:
+        # make sure cloud_id is formatted correctly
+        cloud_id = os.getenv('ElasticSearch_ID')
+        if not cloud_id.endswith("=="):
+            cloud_id += "=="
+        print(cloud_id)
+
+        es = Elasticsearch(
+            cloud_id=cloud_id,
+            basic_auth=("elastic", os.getenv('ElasticSearch_PWD'))
+        )
+
+        # Default values
+        query = kwargs.get('query', '')
+        lat = kwargs.get('lat', None)
+        lon = kwargs.get('lon', None)
+        index = 'bitechat'
+        
+        print(f"Index: {index}, Query: {query}, Latitude: {lat}, Longitude: {lon}")
+
+        model = SentenceTransformer('intfloat/e5-small-v2')
+        vector = model.encode(query, normalize_embeddings=True).tolist()
+        print("Vector created.")
+
+        # Construct the geo_distance filter if lat and lon are provided
+        geo_distance_filter = {}
+        if lat is not None and lon is not None:
+            geo_distance_filter = {
+                "filter": [{
+                    "geo_distance": {
+                        "distance": "2km",
+                        "location": {"lat": lat, "lon": lon}
+                    }
+                }]
+            }
+
+        response = es.search(
+            index=index,
+            size=3,
+            query={"match": {"info": query}},
+            _source=["info", "food", "review_summary"],
+            knn={
+                "field": "review_vector",
+                "query_vector": vector,
+                "k": 5,
+                "num_candidates": 15,
+                **geo_distance_filter
+            },
+            rank={"rrf": {}},
+        )
+
+        results = []
+        for hit in response['hits']['hits']:
+            source = hit['_source']
+            formatted_source = {
+                'info': source.get('info', 'No information available'),
+                'popular_dishes': source.get('food', 'Popular dishes not available'),
+                'review_summary': source.get('review_summary', 'Review summary not available')
+        }
+            results.append(formatted_source)
+
+        return results
+
+    def _arun(self, **kwargs: str) -> List[Dict]:
+        raise NotImplementedError("This tool does not support async")
+
+class AgentManger:
+    def __init__(self, config_path='config.json'):
+
+        with open(config_path, 'r') as f:
+            self.config = json.load(f)
+
+        # LangSmith Configurations
+        self.callbacks = [
+            LangChainTracer(
+                project_name=os.getenv('LANGCHAIN_PROJECT'),
+                client=Client(
+                api_url="https://api.smith.langchain.com",
+                api_key=os.getenv('LANGCHAIN_API_KEY')
+                )
+            )
+            ]
+
+        self.llm = ChatOpenAI(
+            api_key=os.getenv('OPENAI_API_KEY'), 
+            temperature=self.config.get('chat_temperature'),
+            model_name=self.config.get('chat_model'),
+        )
+        self.tools = [CoordinateSearchTool(), ElasticSearchTool()]
+        self.chat_agent = self.create_agent(instruction=self.config.get('chat_instruction'))
+        # self.agent_executor = AgentExecutor(agent=self.chat_agent, tools=self.tools, verbose=True)
+
+    def create_agent(self, instruction):
+        """
+        Create an agent that uses the OpenAI language model and the specified tools.
+
+        Args:
+            llm (ChatOpenAI): The language model to use.
+            tools (List[BaseTool]): The tools to use.
+            prompt (ChatPromptTemplate): The prompt to use.
+
+        Returns:
+            Agent: The created agent.
+        """
+        prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(instruction),
+            MessagesPlaceholder(variable_name="chat_history", optional=True),
+            HumanMessagePromptTemplate.from_template("{input}"), 
+            MessagesPlaceholder(variable_name="agent_scratchpad")
+        ])
+
+        print(prompt)
+
+        agent = create_openai_tools_agent(self.llm, self.tools, prompt)
+        agent_executor = AgentExecutor(agent=agent, tools=self.tools, verbose=True)
+
+        # add history to the agent
+        message_history = ChatMessageHistory()
+        agent_with_chat_history = RunnableWithMessageHistory(
+            agent_executor,
+            lambda session_id: message_history,
+            input_messages_key="input",
+            history_messages_key="chat_history",
+        )
+
+        return agent_with_chat_history
+
+    def chat_with_agent(self, input_message, session_id):
+        """
+        """
+        result = self.chat_agent.invoke(
+            {"input": input_message},
+            config={"configurable": {"session_id": session_id}, "callbacks": self.callbacks}
+        )
+
+        return result
+
+## Initialize the agent manager  
+rag_agent = AgentManger()
+
+def chat(user_message, session_id):
+    return rag_agent.chat_with_agent(user_message, session_id)
